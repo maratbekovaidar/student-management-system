0eeadc83589a55f7222b56bc5536ca501e7b63dd
Yerzat Meirambekuly <77789534+yerzatm002@users.noreply.github.com>
2021-11-20 16:10:23 +0600

    adding Course backup 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,12 +6,6 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-//        String user;
-//        LoginModule loginModule = new LoginModule();
-//        user = loginModule.login();
 
-        CourseService courseService = new CourseService();
-        courseService.setCourse();
-        courseService.getAllCourse();
     }
 }
