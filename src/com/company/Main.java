0e527798d7a8df8be464e80ee76b542aad3f8d4e
Yerzat Meirambekuly <77789534+yerzatm002@users.noreply.github.com>
2021-11-20 15:57:20 +0600

    adding COURSE module backup

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,10 +6,12 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        String user;
-        LoginModule loginModule = new LoginModule();
-        user = loginModule.login();
+//        String user;
+//        LoginModule loginModule = new LoginModule();
+//        user = loginModule.login();
 
-
+        CourseService courseService = new CourseService();
+        courseService.setCourse();
+        courseService.getAllCourse();
     }
 }
